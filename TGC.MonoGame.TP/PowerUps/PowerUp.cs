c0c291fdeb0e5f370d023b8094af8e065f88af7a
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-10-23 03:06:54 -0300

    Refactor logica de PowerUp en la clase abstracta.

--- TGC.MonoGame.TP/PowerUps/PowerUp.cs
+++ TGC.MonoGame.TP/PowerUps/PowerUp.cs
@@ -5,20 +5,46 @@ namespace TGC.MonoGame.TP.PowerUps;
 public abstract class PowerUp
 {
     public BoundingBox BoundingBox { get; protected set; }
-    protected float PowerUpDuration { get; set; }
-    public bool IsPowerUpActive { get; protected set; }
+    protected float PowerUpDuration { get; init; }
+    protected bool IsPowerUpActive { get; set; }
+    private float ElapsedTimeSinceActivation { get; set; }
     protected PowerUp(BoundingBox boundingBox)
     {
         BoundingBox = boundingBox;
     }
 
-    protected void HandlePlayerPowerUp(Player player)
+    protected void HandlePlayerPowerUp(Player player, GameTime gameTime)
     {
+        var elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        
         if (player.BoundingSphere.Intersects(BoundingBox))
         {
-            ApplyPowerUp(player);
+            ActivatePowerUp(player);
+        }
+
+        if (!IsPowerUpActive) return;
+        ElapsedTimeSinceActivation += elapsedTime;
+            
+        if (ElapsedTimeSinceActivation >= PowerUpDuration)
+        {
+            DeactivatePowerUp(player);
         }
     }
+
+    protected abstract void SetPowerUp(Player player);
+    protected abstract void ResetPowerUp(Player player);
+
+    private void ActivatePowerUp(Player player)
+    {
+        SetPowerUp(player);
+        IsPowerUpActive = true;
+    }
+
+    private void DeactivatePowerUp(Player player)
+    {
+        ResetPowerUp(player);
+        IsPowerUpActive = false;
+        ElapsedTimeSinceActivation = 0f;
+    }
     
-    protected abstract void ApplyPowerUp(Player player);
 }
--- TGC.MonoGame.TP/PowerUps/Star.cs
+++ TGC.MonoGame.TP/PowerUps/Star.cs
@@ -8,7 +8,7 @@ public class Star : PowerUp
     public Matrix World { get; private set; }
     private Vector3 Position { get; set; }
     private float Scale { get; }
-    private float _elapsedTimeSinceActivation;
+
     private const float Amplitude = 0.15f;
     private const float MaxVerticalSpeed = 2f;
 
@@ -24,30 +24,20 @@ public class Star : PowerUp
 
     public void Update(GameTime gameTime)
     {
+        HandlePlayerPowerUp(TGCGame.Player, gameTime);
         var time = (float)gameTime.TotalGameTime.TotalSeconds;
-        var elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-        HandlePlayerPowerUp(TGCGame.Player);
         var verticalOffset = Amplitude * (float)Math.Cos(time * MaxVerticalSpeed);
         Position = new Vector3(Position.X, Position.Y + verticalOffset, Position.Z);
         BoundingBox = new BoundingBox(BoundingBox.Min + Vector3.Up * verticalOffset, BoundingBox.Max + Vector3.Up * verticalOffset);
         World = Matrix.CreateScale(Scale) * Matrix.CreateTranslation(Position);
-        
-        if (IsPowerUpActive)
-        {
-            _elapsedTimeSinceActivation += elapsedTime;
-            
-            if (_elapsedTimeSinceActivation >= PowerUpDuration)
-            {
-                TGCGame.Player.ResetGravity();
-                IsPowerUpActive = false;
-                _elapsedTimeSinceActivation = 0f;
-            }
-        }
     }
-    
-    protected override void ApplyPowerUp(Player player)
+
+    protected override void SetPowerUp(Player player)
     {
         player.Gravity = 60f;
-        IsPowerUpActive = true;
+    }
+    protected override void ResetPowerUp(Player player)
+    {
+        player.ResetGravity();
     }
 }
