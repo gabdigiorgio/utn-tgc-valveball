907af7a74c50a596bca385ef4eb7623e5e45d9a8
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-11-08 18:49:52 -0300

    Agrego AudioManager.

---
+++ TGC.MonoGame.TP/Audio/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace TGC.MonoGame.TP.Audio;
+
+public class AudioManager
+{
+    public SoundEffect JumpSound { get; private set; }
+    public SoundEffect RollingSound { get; private set; }
+    public List<SoundEffect> BumpSounds { get; private set; }
+    
+    public SoundEffect OpenMenuSound { get; private set; }
+    public SoundEffect SelectMenuSound { get; private set; }
+    public SoundEffect ClickMenuSound { get; private set; }
+    
+    public Song BackgroundMusic { get; private set; }
+    
+    public void LoadSounds(ContentManager contentManager)
+    {
+        JumpSound = LoadAudio<SoundEffect>(contentManager, "jump");
+        RollingSound = LoadAudio<SoundEffect>(contentManager, "rolling_hard");
+        BumpSounds = LoadAudioList<SoundEffect>(contentManager, 4, i => $"bounce_hard{i}");
+        OpenMenuSound = LoadAudio<SoundEffect>(contentManager, "open_menu");
+        SelectMenuSound = LoadAudio<SoundEffect>(contentManager, "select_menu");
+        ClickMenuSound = LoadAudio<SoundEffect>(contentManager, "click_menu");
+        BackgroundMusic = LoadAudio<Song>(contentManager, "classic_vibe");
+    }
+    
+    private static T LoadAudio<T>(ContentManager contentManager, string audioName)
+    {
+        return contentManager.Load<T>(audioName);
+    }
+    
+    private static List<T> LoadAudioList<T>(ContentManager contentManager, int count, Func<int, string> soundNameFunc)
+    {
+        var soundList = new List<T>();
+
+        for (var i = 1; i <= count; i++)
+        {
+            var soundName = soundNameFunc(i);
+            var sound = LoadAudio<T>(contentManager, soundName);
+            soundList.Add(sound);
+        }
+
+        return soundList;
+    }
+}
--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -5,7 +5,6 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Media;
 using TGC.MonoGame.TP.Cameras;
 using TGC.MonoGame.TP.Collectible;
 using TGC.MonoGame.TP.Collectible.Coins;
