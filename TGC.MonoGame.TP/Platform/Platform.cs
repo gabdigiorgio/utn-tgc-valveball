b20e5bf793cdcbb4538e7956d5c503b55f5068e3
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-11-09 00:28:39 -0300

    Agrego clase Platform.

---
+++ TGC.MonoGame.TP/Platform/Platform.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using TGC.MonoGame.TP.Collisions;
+
+namespace TGC.MonoGame.TP.Platform;
+
+public class Platform : Prefab
+{
+    public BoundingBox BoundingBox { get; set; }
+
+    public Platform(Vector3 scale, Vector3 position) : base(scale, position)
+    {
+        BoundingBox = BoundingVolumesExtensions.FromMatrix(World);
+    }
+}
--- TGC.MonoGame.TP/Platform/Prefab.cs
+++ TGC.MonoGame.TP/Platform/Prefab.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using TGC.MonoGame.TP.Geometries;
 
 namespace TGC.MonoGame.TP.Platform;
 
 public abstract class Prefab
 {
-    public readonly Matrix World =  new();
+    public readonly Matrix World;
     public BoxPrimitive Box { get; set; }
     public Material Material { get; set; }
+
+    protected Prefab(Vector3 scale, Vector3 position)
+    {
+        World = Matrix.CreateScale(scale) * Matrix.CreateTranslation(position);
+    }
+
+    /*public void LoadPrefab(GraphicsDevice graphicsDevice)
+    {
+        Box = new BoxPrimitive(graphicsDevice, Vector3.One, Material.Diffuse);
+    }*/
 }
