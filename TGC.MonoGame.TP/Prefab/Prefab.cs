4b7e9c35fc99b29d0cad399c72956e4c9ea97ede
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2023-11-09 16:10:59 -0300

    Rename package Prefab.

--- TGC.MonoGame.TP/Prefab/Prefab.cs
+++ TGC.MonoGame.TP/Prefab/Prefab.cs
@@ -1,16 +1,14 @@
-﻿using System.Runtime.InteropServices;
-using Microsoft.Xna.Framework;
-using TGC.MonoGame.TP.Collisions;
+﻿using Microsoft.Xna.Framework;
 
 namespace TGC.MonoGame.TP.Platform;
 
 public abstract class Prefab
 {
     public Matrix World;
-    public Vector3 Scale { get; set; }
+    protected Vector3 Scale { get; }
     public Vector3 Position { get; set; }
-    public Vector3? PreviousPosition { get; protected set; } = null;
-    public Material Material { get; set; }
+    public Vector3? PreviousPosition { get; protected set; }
+    public Material Material { get; }
     
     public abstract bool Intersects(BoundingSphere sphere);
     
